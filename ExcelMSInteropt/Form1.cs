72cf62ce06dd5d0c10adc12c559b80686e12c192
ivolgin0349 <ivolgin0349@gmail.com>
2018-12-29 13:37:25 +0300

    Добавьте файлы проекта.

---
+++ ExcelMSInteropt/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel; 
+
+namespace ExcelMSInteropt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // параметра передать Missing.Value
+            System.Reflection.Missing missingValue = System.Reflection.Missing.Value;
+
+            //создаем и инициализируем объекты Excel
+            Excel.Application App;
+            Excel.Workbook xlsWB;
+            Excel.Worksheet xlsSheet;
+
+            App = new Microsoft.Office.Interop.Excel.Application();
+            //добавляем в файл Excel книгу. Параметр в данной функции - используемый для создания книги шаблон.
+            //если нас устраивает вид по умолчанию, то можно спокойно передавать пустой параметр.
+            xlsWB = App.Workbooks.Add(missingValue);
+            //и использует из нее
+            xlsSheet = (Excel.Worksheet)xlsWB.Worksheets.get_Item(1);
+
+            List<string[]> rows = new List<string[]>
+            {
+                new string[] { "1", "2", "3", "4" },
+                new string[] { "5", "6", "7", "8" },
+                new string[] { "9", "10", "11", "12" }
+            };
+            for (int i = 0; i < rows.Count; i++)
+            {
+                for (int j = 0; j < rows[i].Length; j++)
+                {
+                    xlsSheet.Cells[i + 1, j + 1] = rows[i][j];
+                }
+            }
+
+            //у SaveAs масса  параметров, некоторые из которых могут оказаться для вас полезными. 
+            //Сейчас мы используем только тип формата Excel12 (Excel 97) и права доступа.
+            //Полное описание что и как смотрите на MSDN: 
+            // http://msdn.microsoft.com/ru-ru/library/microsoft.office.tools.excel.workbook.saveas.aspx
+            xlsWB.SaveAs(@"C:\Users\i.geraskin\source\repos\excel_text.xls",
+                         Excel.XlFileFormat.xlExcel12,
+                         missingValue,
+                         missingValue,
+                         missingValue,
+                         missingValue,
+                         Excel.XlSaveAsAccessMode.xlNoChange,
+                         missingValue,
+                         missingValue,
+                         missingValue,
+                         missingValue,
+                         missingValue);
+            //закрываем книгу                                                                        
+            xlsWB.Close(true, missingValue, missingValue);
+            //закрываем приложение
+            App.Quit();
+
+            MessageBox.Show("Все сохранено", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //уменьшаем счетчики ссылок на COM объекты, что, по идее должно их освободить.
+            //почему это не произойдет - читайте ниже ;)
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlsSheet);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(xlsWB);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(App);
+
+        }
+    }
+}
---
+++ ExcelMSInteropt/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ExcelMSInteropt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExcelMSInteropt")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("156b824c-2706-4538-9de7-018dfbd82a9e")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
